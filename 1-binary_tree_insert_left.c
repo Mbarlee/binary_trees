8e870adcfd78c64e7238f3d9ae293783160a6389
Mbali <shezi7314@gmail.com>
2023-03-07 23:42:41 -0800

    insert

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,34 +1,48 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_insert_left - Function that inserts a node as the
- * left-child of another node
- * @parent: Pointer to the node to insert the left-child in
- * @value: Value to store in the new node.
- * Return: A pointer to the created node, or NULL on failure or
- * if parent is NULL
+ * insert_izq - aux function.
+ * @a_insertar: idk.
+ * @padre: idk.
+ */
+void insert_izq(binary_tree_t *a_insertar, binary_tree_t *padre)
+{
+	padre->left = a_insertar;
+	if (a_insertar)
+		a_insertar->parent = padre;
+}
+
+/**
+ * remover_izq - aux function.
+ * @a_remover: idk.
+ * Return: idk.
+ */
+binary_tree_t *remover_izq(binary_tree_t *a_remover)
+{
+	a_remover->parent->left = NULL;
+	a_remover->parent = NULL;
+	return (a_remover);
+}
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of a node.
+ * @parent: pointer to the node to insert the left-child in.
+ * @value: value to store in the new node.
+ * Return: pointer to the new node, or NULL on failure or if parent is NULL.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_left;
+	binary_tree_t *node = NULL;
+	binary_tree_t *aux = NULL;
 
-	if (parent == NULL)
-	{
+	if (!parent)
 		return (NULL);
-	}
-
-	new_left = binary_tree_node(parent, value);
-	if (!new_left)
-	{
+	node = binary_tree_node(NULL, value);
+	if (!node)
 		return (NULL);
-	}
-
 	if (parent->left)
-	{
-		new_left->left = parent->left;
-		parent->left->parent = new_left;
-	}
-
-	parent->left = new_left;
-
-	return (new_left);
+		aux = remover_izq(parent->left);
+	insert_izq(node, parent);
+	insert_izq(aux, node);
+	return (node);
 }
